540047a3751a7ae8a051edb7549a2a9db7833e6e
Vinayak <vinayakdeepu@gmail.com>
2021-05-03 10:36:02 +0530

    Add shader file import in webpack config

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -1,88 +1,78 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
-const path = require('path')
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, '../src/index.js'),
-  output:
-  {
-    filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, '../dist')
+  entry: path.resolve(__dirname, "../src/index.js"),
+  output: {
+    filename: "bundle.[contenthash].js",
+    path: path.resolve(__dirname, "../dist"),
   },
-  devtool: 'source-map',
-  plugins:
-    [
-      new CopyWebpackPlugin({
-        patterns: [
-          { from: path.resolve(__dirname, '../static') }
-        ]
-      }),
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, '../src/index.html'),
-        minify: true
-      }),
-      new MiniCSSExtractPlugin()
+  devtool: "source-map",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: path.resolve(__dirname, "../static") }],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../src/index.html"),
+      minify: true,
+    }),
+    new MiniCSSExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      // html
+      {
+        test: /\.(html)$/,
+        use: ["html-loader"],
+      },
+
+      // js
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+
+      // css
+      {
+        test: /\.css$/,
+        use: [MiniCSSExtractPlugin.loader, "css-loader"],
+      },
+
+      // images
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/images/",
+            },
+          },
+        ],
+      },
+
+      // fonts
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets/fonts/",
+            },
+          },
+        ],
+      },
+
+      // shaders
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ["raw-loader"],
+      },
     ],
-  module:
-  {
-    rules:
-      [
-        // HTML
-        {
-          test: /\.(html)$/,
-          use: ['html-loader']
-        },
-
-        // JS
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use:
-            [
-              'babel-loader'
-            ]
-        },
-
-        // CSS
-        {
-          test: /\.css$/,
-          use:
-            [
-              MiniCSSExtractPlugin.loader,
-              'css-loader'
-            ]
-        },
-
-        // Images
-        {
-          test: /\.(jpg|png|gif|svg)$/,
-          use:
-            [
-              {
-                loader: 'file-loader',
-                options:
-                {
-                  outputPath: 'assets/images/'
-                }
-              }
-            ]
-        },
-
-        // Fonts
-        {
-          test: /\.(ttf|eot|woff|woff2)$/,
-          use:
-            [
-              {
-                loader: 'file-loader',
-                options:
-                {
-                  outputPath: 'assets/fonts/'
-                }
-              }
-            ]
-        }
-      ]
-  }
-}
+  },
+};
